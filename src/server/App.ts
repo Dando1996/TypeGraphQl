2ec050b2ada2225d0a0416617a5f2a167d6b27b8
dando1996 <jjddjjmmb@hotmail.co.uk>
2019-03-24 20:35:05 +0000

    Refactored App

--- src/server/App.ts
+++ src/server/App.ts
@@ -1,22 +1,50 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
 
 class App {
-  public express: any
+  public expressApp: any
+  public port: number
 
-  constructor () {
-    this.express = express();
+  constructor (port: number, middleWares?: any) {
+    this.expressApp = express()
+    this.initializeMiddlewares(middleWares)
     this.mountRoutes()
+    this.port = port
   }
 
   private mountRoutes (): void {
     const router = express.Router()
+
     router.get('/', (req, res) => {
       res.json({
         message: 'Hello World!'
       })
     })
-    this.express.use('/', router)
+
+    router.post('/', (req, res) => {
+      res.send(req.body)
+    })
+
+    this.expressApp.use('/api', router)
+  }
+  private initializeMiddlewares(middleWares?: any) {
+    this.expressApp.use(bodyParser.json())
+  }
+
+  private initializeControllers(controllers) {
+    controllers.forEach((controller) => {
+      this.expressApp.use('/', controller.router)
+    });
+  }
+
+  public listen(port?: number, err?: any) {
+    if (err) {
+      return console.log(err)
+    }
+    this.expressApp.listen(this.port, () => {
+      console.log(`App listening on the port ${this.port}`)
+    });
   }
 }
-
-export default new App().express
+export default App;
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,4 @@
 import App from './App'
 
-const port = process.env.PORT || 3001
-
-App.listen(port, (err) => {
-  if (err) {
-    return console.log(err)
-  }
-
-  return console.log(`server is listening on ${port}`)
-})
+const app = new App(3001)
+app.listen()
