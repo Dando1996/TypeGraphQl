6b05a5f6160165e85429b8a71ed4c4f1d7dab3cb
dando1996 <jjddjjmmb@hotmail.co.uk>
2019-03-26 16:11:56 +0000

    Updated env variable name

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,7 +4,7 @@ import 'dotenv/config';
 const env = process.env.NODE_ENV;
 console.log('env: ' + env)
 
-const port = parseInt(process.env.PORT) || 3001;
+const port = parseInt(process.env.SERVER_PORT) || 3001;
 console.log('port: ' + port)
 
 const app = new App(port)
