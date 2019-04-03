fa8bb49e01be1f4749b4318ea8d12c813fd29247
dando1996 <jjddjjmmb@hotmail.co.uk>
2019-04-03 23:03:31 +0100

    Connected via mongoose, added controller setup

--- src/server/App.ts
+++ src/server/App.ts
@@ -1,18 +1,23 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import { MongoClient } from 'mongodb';
+import * as mongoose from 'mongoose';
+import Controller from './Controllers/types';
+import errorMiddleware from './Middleware/error.middleware';
+import ProductController from './Controllers/ProductController';
 
 class App {
   public expressApp: any
   public port: number
   public mongoClient: MongoClient
 
-  constructor (port: number, middleWares?: any) {
+  constructor (port: number, middleWares?: any, controllers?: Controller[]) {
     this.expressApp = express()
     this.initializeMiddlewares(middleWares)
+    this.initializeControllers(controllers)
+    this.initializeErrorHandling()
     this.mountRoutes()
     this.port = port
-    this.mongoClient = this.initializeMongoClient()
+    this.mongoConnection = this.initializeMongoConnection()
   }
 
   private mountRoutes (): void {
@@ -30,25 +35,33 @@ class App {
 
     this.expressApp.use('/api', router)
   }
+
   private initializeMiddlewares(middleWares?: any) {
     this.expressApp.use(bodyParser.json())
   }
 
-  private initializeControllers(controllers) {
-    controllers.forEach((controller) => {
-      this.expressApp.use('/', controller.router)
-    });
+  private initializeControllers(controllers?: Controllers[]) {
+    if (controllers !== undefined || controllers.length != 0){
+      controllers.forEach((controller) => {
+        this.expressApp.use('/api', controller.router)
+      });
+    }
   }
 
-  public initializeMongoClient() {
+  public initializeMongoConnection() {
     const {
       MONGO_USER,
       MONGO_PWD,
       MONGO_PATH,
-      MP_LAB_CLUSTER
+      MONGO_DB
     } = process.env;
-    const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PWD}@${MONGO_PATH}/test?retryWrites=true`;
-    return new MongoClient(uri, { useNewUrlParser: true });
+    const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PWD}@${MONGO_PATH}/${MONGO_DB}?retryWrites=true`;
+    return mongoose.connect(uri, { useNewUrlParser: true });
+    //our models will use the default connection as they are created by mongoose.model()
+  }
+
+  public initializeErrorHandling() {
+    this.expressApp.use(errorMiddleware)
   }
 
   public listen(port?: number, err?: any) {
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,7 @@
 import App from './App'
 import 'dotenv/config';
-
+import ProductController from './Controllers/ProductController';
+import ProductSchema from './Schemas/ProductSchema';
 const assert = require('assert');
 
 const env = process.env.NODE_ENV;
@@ -9,13 +10,7 @@ console.log('env: ' + env)
 const port = parseInt(process.env.SERVER_PORT) || 3001;
 console.log('port: ' + port)
 
-const app = new App(port)
-app.mongoClient.connect(err => {
-  assert.equal(null, err);
-  console.log('successfully connected to db');
-  const collection = app.mongoClient.db("BlackMarket").collection("Items");
-  // perform actions on the collection object
-  app.mongoClient.close();
-});
+const productController = new ProductController();
+const app = new App(port, undefined, [productController])
 
 app.listen(port)
